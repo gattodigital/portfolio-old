858919e641a0e8bf7f16a453502d476b5c759c15
Gatto Digital <andy@gattodigital.com>
2017-06-20 10:28:57 -0500

    revision round 1

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -16,7 +16,7 @@ $phone = $_POST['phone'];
 $message = $_POST['message'];
 	
 // Create the email and send the message
-$to = 'andres@websolutmiami.com'; // Add your email address in between the ''
+$to = 'contact@andresramirez.website'; // Add your email address in between the ''
 $email_subject = "AR Web Design:  $name";
 $email_body = "You have received a new message from your andresramirez.website.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 $headers = "From: email@andresramirez.website\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
